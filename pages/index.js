ebfdd19280b36242c619f326bc9d8bc1d0174554
EIBelous <evgen.i.bilous@gmail.com>
2022-11-03 07:06:28 +0200

    react project

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import {data} from "..speakerData.js"
+ 
+const indexPage = ()=>{
+    return (
+        const {id,bio,first,last,favorite,twitterHandle,company,sessions,}=data[0]
+
+        <div>Hello from Plural</div>
+    );
+}
+
+export default indexPage
