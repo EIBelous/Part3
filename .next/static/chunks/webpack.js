ce2c7cc1f3cf3c4eb70a8f7f5351f4215c26b901
EIBelous <evgen.i.bilous@gmail.com>
2022-11-21 07:02:45 +0200

    create Speaker element

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "291684fff8f035b4"; }
+/******/ 		__webpack_require__.h = function() { return "b76c222fcf5e26c8"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
