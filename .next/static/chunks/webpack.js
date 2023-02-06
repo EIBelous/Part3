86c6153bb8d30f1c995fdbe7a51d55d3fd1732f1
EIBelous <evgen.i.bilous@gmail.com>
2023-02-06 15:47:07 +0200

    map all speakers

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b76c222fcf5e26c8"; }
+/******/ 		__webpack_require__.h = function() { return "649e1868004e1604"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
