38affeb2a595dc3913bd4ffded2b8a338f604609
EIBelous <evgen.i.bilous@gmail.com>
2023-03-14 16:09:13 +0200

    handing async loading

--- src/components/SpeakersList.js
+++ src/components/SpeakersList.js
@@ -1,13 +1,23 @@
 import Speaker from './Speaker'
 import {data} from "../../SpeakerData"
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 function SpeakersList({showSessions}){
-    const [speakersData,setSpeakersData]=useState(data)
+    const [speakersData,setSpeakersData]=useState([]);
+
+    const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
+
+    useEffect(() => {
+        async function delayFunk(){
+        await delay(1000);
+        setSpeakersData(data); 
+        }
+        delayFunk()
+    },[])
 
 function onFavoriteToggle(id){
-    const speakersRecPrevious=speakersData.find(function(rec){
-        return rec.id === id
+    const speakersRecPrevious = speakersData.find(function(speaker){
+        return speaker.id === id
     })
     const speakersRecUpdated={
         ...speakersRecPrevious,
