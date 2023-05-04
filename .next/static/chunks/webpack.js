792b09d5d1c3f30b2ca142aa4095af31249e78bc
EIBelous <evgen.i.bilous@gmail.com>
2023-05-04 13:35:42 +0300

    1

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c3b60e71c4bcaebd"; }
+/******/ 		__webpack_require__.h = function() { return "9cd3666d876bf5b0"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
---
+++ next.config.js
@@ -0,0 +1,8 @@
+
+
+// module.exports = {
+//     experiments: {
+//       topLevelAwait: true,
+//     },
+//   };
+  
