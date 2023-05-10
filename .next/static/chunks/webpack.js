d1e6d6c44da6827ab43587bcb6cb6db39cc8c32e
EIBelous <evgen.i.bilous@gmail.com>
2023-05-10 16:48:22 +0300

    implement res, put, dele, [pst1

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e82b4014e8689cbf"; }
+/******/ 		__webpack_require__.h = function() { return "cbaa664c73832349"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
